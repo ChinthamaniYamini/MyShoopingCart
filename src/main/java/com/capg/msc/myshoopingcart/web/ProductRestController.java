1ab62ad66c185d86209d697cf5194c358ea6e75e
ChinthamaniYamini <82094829+ChinthamaniYamini@users.noreply.github.com>
2021-05-15 13:37:12 +0530

    Update ProductRestController.java

--- src/main/java/com/capg/msc/myshoopingcart/web/ProductRestController.java
+++ src/main/java/com/capg/msc/myshoopingcart/web/ProductRestController.java
@@ -3,9 +3,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
+
+
 import java.time.LocalDateTime;
 import java.util.List;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,14 +21,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.capg.msc.myshoopingcart.beans.Product;
+import com.capg.msc.myshoopingcart.exception.InvalidCategoryException;
 import com.capg.msc.myshoopingcart.exception.InvalidCostRangeException;
-import com.capg.msc.myshoopingcart.exception.InvalidCategoryException
-
+//import com.capg.msc.myshoopingcart.dao.CustomProductRepositoryImp;
 import com.capg.msc.myshoopingcart.service.ProductServiceImp;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 @Validated
 @RestController
 @RequestMapping("/api")
+@Api(value = "Online Laptop Store",description = "Various api works on laptop inventory management")
 public class ProductRestController {
 
 	@Autowired
@@ -40,14 +45,14 @@ public class ProductRestController {
 		return "WELCOME : MY SHOPPING APP "+LocalDateTime.now();
 	}
 	
-	
+	@ApiOperation(value = "product post mapping" , response = Product.class)
 	@PostMapping("/product")
 	public Product insertProduct(@RequestBody @Valid Product p) {
 		service.SaveProduct(p);
 		return p;
 	}
 	
-	
+	@ApiOperation(value = "product Get mapping to fatch all products" , response = List.class)
 	@GetMapping("/products")
 	public List<Product> getProducts(){
 		return service.getAllProducts();
@@ -77,12 +82,14 @@ public class ProductRestController {
 	}
 	
 	@GetMapping("/products/{proudctCategory}")
-	public List<Product> getProductByCategory(@PathVariable String proudctCategory) {
+	public List<Product> getProductByCategory(@PathVariable String proudctCategory) throws  InvalidCategoryException{
+		
 		Matcher m = Pattern.compile("[0-9]|@|[\\+-x\\*]").matcher(proudctCategory);
 		
 		if(proudctCategory.length()<3 || m.find()) {
 			throw new InvalidCategoryException(proudctCategory);
 		}
+		
 		return service.getProductByCategory(proudctCategory);
 	}
 	
